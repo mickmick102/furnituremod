40131e3f43f9070032aafca56929be42e7e00c46
Michaël Côté <michaelc1998@hotmail.ca>
2021-08-28 00:05:28 -0400

    Add diffrent wood type of gouvernailles.

--- src/main/java/com/mickmick102/furnituremod/init/BlockInit.java
+++ src/main/java/com/mickmick102/furnituremod/init/BlockInit.java
@@ -13,5 +13,12 @@ public class BlockInit {
 
     //BLOCKS
     public static final Block BENCH_BLOCK = new BlockBench("bench_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
-    public static final Block BARRE_BLOCK = new BlockBarre("barre_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
+
+    // Barre block
+    public static final Block OAK_BARRE_BLOCK = new BlockBarre("oak_barre_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
+    public static final Block SPRUCE_BARRE_BLOCK = new BlockBarre("spruce_barre_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
+    public static final Block BIRCH_BARRE_BLOCK = new BlockBarre("birch_barre_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
+    public static final Block JUNGLE_BARRE_BLOCK = new BlockBarre("jungle_barre_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
+    public static final Block ACACIA_BARRE_BLOCK = new BlockBarre("acacia_barre_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
+    public static final Block DARK_OAK_BARRE_BLOCK = new BlockBarre("dark_oak_barre_block", Material.WOOD, 10.0f, 10.0f, 2, "axe");
 }
